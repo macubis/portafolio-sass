8d63908d101df7560886de47348eefe40fbb1f63
macubis <macubis@gmail.com>
2017-07-23 11:14:48 -0400

    Subiendo maqueta

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+	$(".dropdown-button").dropdown();
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	$('input#input_text, textarea#textarea1').characterCounter();
+ 
+        
+
+})
